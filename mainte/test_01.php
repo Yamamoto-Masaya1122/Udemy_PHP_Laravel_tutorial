cbd1b3853bf7410540f69398f4dbc980e5e95e90
Masaya Yamamoto <masaya1122baseball@gmail.com>
2024-01-05 18:52:43 +0900

    1月5日PHPの学習

--- mainte/test_01.php
+++
@@ -1,8 +0,0 @@
-<?php
-// パスワードを記載したファイルの場所
-echo __FILE__;
-// /Applications/MAMP/htdocs/php_test/mainte/.htpasswd
-// パスワード(暗号化)
-echo '<br>';
-echo(password_hash('password123', PASSWORD_BCRYPT));
-?>
