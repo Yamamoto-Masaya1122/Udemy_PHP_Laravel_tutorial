3594d72fbc7f789784c55001ad3fa7cbe3f7c55e
Masaya Yamamoto <masaya1122baseball@gmail.com>
2024-01-13 15:35:19 +0900

    1月13日Laravelの学習

--- laravel/task_test/app/Http/Controllers/TestController.php
+++ laravel/task_test/app/Http/Controllers/TestController.php
@@ -4,14 +4,29 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Test;
+use Illuminate\Support\Facades\DB;
 
 class TestController extends Controller
 {
     public function index()
     {
+
+        // Eloquent(エロクアント)
         $values = Test::all();
 
-        //dd($values);
+        $count = Test::count();
+
+        $first = Test::findOrFail(1);
+
+        // whereの場合は、getメソッドをつけないとデータを取ってこれない。その時はbuilder型で表示される。(クエリの途中ということ)
+        $whereAAA = Test::where('text', '=', 'aaa')->get();
+
+        // クエリビルダ
+        $queryBuilder = DB::table('tests')->where('text', '=' , 'aaa')
+        ->select('id', 'text')
+        ->get();
+
+        dd($values, $count, $first, $whereAAA, $queryBuilder);
 
         return view('tests.test', compact('values'));
     }
