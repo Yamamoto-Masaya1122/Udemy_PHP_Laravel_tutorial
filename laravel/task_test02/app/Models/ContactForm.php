5cfe1331d275a8bad0ddf41d0b1d00d62a815e8c
Masaya Yamamoto <masaya1122baseball@gmail.com>
2024-03-15 18:17:52 +0900

    3月15日Laravelの学習

--- laravel/task_test02/app/Models/ContactForm.php
+++ laravel/task_test02/app/Models/ContactForm.php
@@ -19,4 +19,14 @@ class ContactForm extends Model
         'contact',
     ];
 
+    public function scopeSearch($query, $search)
+    {
+    if($search !== null){
+        $search_split = mb_convert_kana($search, 's');
+        $search_split2 = preg_split('/[\s]+/', $search_split);
+        foreach($search_split2 as $value){
+            $query->where('name', 'like', '%' .$value. '%'); }
+    }
+    return $query;
+    } 
 }
